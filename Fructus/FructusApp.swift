6aa633836e7285de38915f4d167f402e5e355e3b
Joep Hinderink <hello@joephinderink.nl>
2020-12-25 15:10:52 +0100

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Joep Hinderink on 25/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
